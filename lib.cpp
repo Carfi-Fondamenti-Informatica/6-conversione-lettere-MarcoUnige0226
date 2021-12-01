eefd99f50daf7a6a10e374f277b7b02479d2739d
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-01 22:41:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
 #include "conversione.h"
 
-bool inverter(char n, char &convertito1, char &convertito2){
-    if (n>=65 && n<=90){
-        convertito1=n+32;
-        return true;
-    }
-    else if (n>=97 && n<=122){
-        convertito2=n-32;
-        return true;
-    }
-    else{
+char convertecarattere(char c){
+    char risultato;
+    if (c>='A'&& c<='Z'){
+        risultato=c+32;
+    }else if (c>='a'&& c<='z'){
+        risultato=c-32;
+        return risultato;
+    }else{
         return false;
-    }
+            }
 }
