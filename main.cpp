3db71676f385083f6d896a22d5d80e5266a1dc67
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-01 22:20:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "conversione.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+char n; char convertito1=0; char convertito2=0;
+cin>>n;
+    if (inverter(n,convertito1,convertito2)){
+        cout<<convertito1;
+        cout<<convertito2;
+    }else {
+        cout<<"errore";
+    }
+    return 0;
 }
