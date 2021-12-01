0f065b7733c6165290bbda5c20c0e55c665705df
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-01 22:21:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include "conversione.h"
 
+bool inverter(char n, char &convertito1, char &convertito2){
+    if (n>=65 && n<=90){
+        convertito1=n+32;
+        return true;
+    }
+    else if (n>=97 && n<=122){
+        convertito2=n-32;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
