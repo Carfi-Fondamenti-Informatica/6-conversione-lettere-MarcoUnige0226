33bc8d00ad890d56297e1c70772bc09c1ef81135
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-01 22:40:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main() {
-char n; char convertito1=0; char convertito2=0;
+char n;
+char x;
 cin>>n;
-    if (inverter(n,convertito1,convertito2)){
-        cout<<convertito1;
-        cout<<convertito2;
+x= convertecarattere(n);
+    if (convertecarattere(n)){
+        cout<<x;
     }else {
         cout<<"errore";
     }
