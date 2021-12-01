493533b47c03b6a1d612fdfea64ee3af5d6fe452
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-01 22:40:44 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,5 +1,4 @@
 #ifndef ESERCITAZIONE6_CONVERSIONE_H
 #define ESERCITAZIONE6_CONVERSIONE_H
-bool inverter(char,char &,char &);
+char convertecarattere(char c);
 #endif //ESERCITAZIONE6_CONVERSIONE_H
-
