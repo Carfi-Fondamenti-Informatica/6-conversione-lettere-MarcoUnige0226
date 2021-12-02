1d0ba44e661c0b14e3a5174d5022701def0e27af
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-02 01:27:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 #include "conversione.h"
 
-char convertecarattere(char n){
-    char risultato;
+char cc(char n,char &ris){
     if (n>='A'&& n<='Z'){
-        risultato=n+32;
-        return risultato;
+        ris=n+32;
+        return true;
+    }else if (n>='a'&& n<='z'){
+        ris=n-32;
+        return true;
     }
-    else if (n>='a'&& n<='z'){
-        risultato=n-32;
-    return risultato;
-    }else
-    return false;
+    else{
+        return false;
+    }
+
 
 }
